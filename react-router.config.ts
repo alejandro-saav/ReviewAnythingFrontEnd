5ca9e5c53cad6ee7a426b6b0e6f528913ea7b903
Alejandro Castro <alejandroTPD@live.com>
2026-05-06 04:38:32 -0500

    added robots.txt

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,24 +1,23 @@
 import type { Config } from "@react-router/dev/config";
-import { BASE_API_URL } from "./src/utils/const";
 import { GetLatestReviewIds } from "./src/services/ReviewService";
 import { GetLatestUserIds } from "./src/services/UserService";
 export default {
     appDirectory: "src/app",
     ssr: true,
 
-    async prerender() {
-        let reviewRoutes: string[] = [];
-        let userRoutes: string[] = [];
-        const reviewIdsResponse = await GetLatestReviewIds();
-        const userIdsResponse = await GetLatestUserIds();
-        if (reviewIdsResponse != null) {
-            reviewRoutes = reviewIdsResponse.map((r: number) => `/review/${r}`);
-        }
-        if (userIdsResponse != null) {
-            userRoutes = userIdsResponse.map((u: number) => `user/${u}`);
-        }
-        return ["/", "explorecategories", "login", "signup", "email-confirmation-required", "confirm-email", ...reviewRoutes, ...userRoutes]
-    },
+    // async prerender() {
+    //     let reviewRoutes: string[] = [];
+    //     let userRoutes: string[] = [];
+    //     const reviewIdsResponse = await GetLatestReviewIds();
+    //     const userIdsResponse = await GetLatestUserIds();
+    //     if (reviewIdsResponse != null) {
+    //         reviewRoutes = reviewIdsResponse.map((r: number) => `/review/${r}`);
+    //     }
+    //     if (userIdsResponse != null) {
+    //         userRoutes = userIdsResponse.map((u: number) => `user/${u}`);
+    //     }
+    //     return ["/", "explorecategories", "login", "signup", "email-confirmation-required", "confirm-email", ...reviewRoutes, ...userRoutes]
+    // },
 } satisfies Config;
 
 
